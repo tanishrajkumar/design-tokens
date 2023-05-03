41d9b5d52f0b9a9a0f4471d6a207e00f53f71a7d
tanishrajkumar <tanishraj.k@bsfdv.com>
2023-05-03 21:08:17 +0400

    Auto Merge

--- .github/workflows/create-pr.sh
+++ .github/workflows/create-pr.sh
@@ -20,12 +20,17 @@ git checkout -b $NEW_BRANCH_NAME
 git add .
 git commit -m "Add new changes"
 
-# # Set the remote URL using the access token
+# Set the remote URL using the access token
 git remote set-url origin https://${GH_TOKEN}@github.com/tanishrajkumar/design-tokens/
 
-# # Push the new branch
+# Push the new branch
 git push origin $NEW_BRANCH_NAME
 
-# # Create a pull request using 'gh'
+# Create a pull request using 'gh'
 gh auth login --with-token <<< "${GH_TOKEN}"
 gh pr create --title "$PR_TITLE" --body "$PR_BODY" --base main --head $NEW_BRANCH_NAME
+
+# merge the pull request
+PR_NUMBER=$(gh pr list --state open --base main --head "$(git symbolic-ref --short HEAD)" --json number -q '.[0].number')
+echo "Pull request number: $PR_NUMBER"
+gh pr merge $PR_NUMBER --squash
