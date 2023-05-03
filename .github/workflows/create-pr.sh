3bbc846a32697550784372dced7af290385677aa
tanishrajkumar <tanishraj.k@bsfdv.com>
2023-05-03 18:59:58 +0400

    Updated

--- .github/workflows/create-pr.sh
+++ .github/workflows/create-pr.sh
@@ -23,7 +23,7 @@ git add .
 git commit -m "Add new changes"
 
 # Set the remote URL using the access token
-git remote set-url origin https://github.com/tanishrajkumar/design-tokens.git
+git remote set-url origin https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/
 
 # Push the new branch
 git push origin $NEW_BRANCH_NAME
