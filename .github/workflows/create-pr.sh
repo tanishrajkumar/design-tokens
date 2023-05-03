611a1c36fa76aa18e815b021972f206d39961216
tanishrajkumar <tanishraj.k@bsfdv.com>
2023-05-03 23:03:20 +0400

    Updated Current Branch

--- .github/workflows/create-pr.sh
+++ .github/workflows/create-pr.sh
@@ -5,7 +5,7 @@ GIT_USER_NAME="tanishrajkumar"
 GIT_USER_EMAIL="tanishraj.k@bsfdv.com"
 
 # Set the new branch name and PR title
-NEW_BRANCH_NAME="new-feature-branch"
+CURRENT_BRANCH="style-dictionary"
 PR_TITLE="New changes"
 PR_BODY="Figma tokens has been updated"
 
@@ -14,7 +14,7 @@ git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
 
 # Create a new branch
-git checkout -b $NEW_BRANCH_NAME
+git checkout $CURRENT_BRANCH
 
 # Add and commit changes
 git add .
@@ -25,11 +25,11 @@ git remote set-url origin https://${GH_TOKEN}@github.com/tanishrajkumar/design-t
 
 # Push the new branch
 git pull origin main
-git push origin $NEW_BRANCH_NAME
+git push origin $CURRENT_BRANCH
 
 # Create a pull request using 'gh'
 gh auth login --with-token <<< "${GH_TOKEN}"
-gh pr create --title "$PR_TITLE" --body "$PR_BODY" --base main --head $NEW_BRANCH_NAME
+gh pr create --title "$PR_TITLE" --body "$PR_BODY" --base main --head $CURRENT_BRANCH
 
 # merge the pull request
 PR_NUMBER=$(gh pr list --state open --base main --head "$(git symbolic-ref --short HEAD)" --json number -q '.[0].number')
