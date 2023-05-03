27b6105ab127271ab91d3d631084929799be34f6
tanishrajkumar <tanishraj.k@bsfdv.com>
2023-05-03 19:19:53 +0400

    TOken status updated

--- .github/workflows/create-pr.sh
+++ .github/workflows/create-pr.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-env:
-    GH_TOKEN: ${{ github.token }}
-
 # Set your GitHub username and email
 GIT_USER_NAME="tanishrajkumar"
 GIT_USER_EMAIL="tanishraj.k@bsfdv.com"
 
 # Set your GitHub Personal Access Token
-GH_PERSONAL_ACCESS_TOKEN="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"
+GH_TOKEN="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"
 
 # Set the new branch name and PR title
 NEW_BRANCH_NAME="new-feature-branch"
@@ -28,13 +25,13 @@ git commit -m "Add new changes"
 # Set the remote URL using the access token
 REPO_OWNER=$(git config --get remote.origin.url | sed -E 's/.*:([^/]+)\/.*/\1/')
 REPO_NAME=$(git config --get remote.origin.url | sed -E 's/.*\/([^/]+)\.git/\1/')
-echo "REPO_OWNDER = $REPO_OWNER"
+echo "=============REPO_OWNDER = $REPO_OWNER"
 echo "REPO_NAME = $REPO_NAME"
-git remote set-url origin https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/${REPO_OWNER}/${REPO_NAME}.git
+git remote set-url origin https://${GH_TOKEN}@github.com/${REPO_OWNER}/${REPO_NAME}.git
 
 # Push the new branch
 git push origin $NEW_BRANCH_NAME
 
 # Create a pull request using 'gh'
-gh auth login --with-token <<< "${GH_PERSONAL_ACCESS_TOKEN}"
+gh auth login --with-token <<< "${GH_TOKEN}"
 gh pr create --title "$PR_TITLE" --base main --head $NEW_BRANCH_NAME
