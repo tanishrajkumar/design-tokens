2187284022cb006b7daa4f6b269f26f0380f4ba5
tanishrajkumar <tanishraj.k@bsfdv.com>
2023-05-04 17:03:33 +0400

    Optimization

--- .github/workflows/create-pr.sh
+++ .github/workflows/create-pr.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Set the new branch name and PR title
-NEW_FEATURE_BRANCH="feat/style-dictionary"
-PR_TITLE="feat: figma design tokens changes"
-PR_BODY="Figma design tokens are updated."
-
 # Configure Git
 git config --global user.name "${GH_USER_NAME}"
 git config --global user.email "${GH_USER_EMAIL}"
 
 # Create a new branch
-git checkout -b $NEW_FEATURE_BRANCH
+git checkout -b ${NEW_FEATURE_BRANCH}
 
 # Add and commit changes
 git add .
@@ -20,11 +15,11 @@ git commit -m "Add new changes"
 git remote set-url origin https://${GH_TOKEN}@github.com/tanishrajkumar/design-tokens/
 
 # Push the new branch
-git push -f origin $NEW_FEATURE_BRANCH
+git push -f origin ${NEW_FEATURE_BRANCH}
 
 # Create a pull request using 'gh'
 gh auth login --with-token <<< "${GH_TOKEN}"
-gh pr create --title "$PR_TITLE" --body "$PR_BODY" --base main --head $NEW_FEATURE_BRANCH
+gh pr create --title "${PR_TITLE}" --body "${PR_BODY}" --base main --head ${NEW_FEATURE_BRANCH}
 
 # merge the pull request
 PR_NUMBER=$(gh pr list --state open --base main --head "$(git symbolic-ref --short HEAD)" --json number -q '.[0].number')
